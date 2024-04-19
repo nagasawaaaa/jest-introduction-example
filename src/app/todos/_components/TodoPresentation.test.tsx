a8d079fe1e3f316cffa6b46cfa39740857b4c967
Satoshi Nagasawa <nagasawa@ham.works>
2024-04-19 16:20:53 +0900

    fix: TodoPresentationに渡す配列の修正

--- src/app/todos/_components/TodoPresentation.test.tsx
+++ src/app/todos/_components/TodoPresentation.test.tsx
@@ -5,15 +5,7 @@ import TodoPresentation from './TodoPresentation';
 
 describe('Todo一覧ページのテスト', () => {
   test('Todo一覧ページが表示されること', () => {
-    const mockTodos: Todo[] = [
-      {
-        userId: 1,
-        id: 1,
-        title: 'delectus aut autem',
-        completed: false,
-      },
-    ];
-    const { getByText } = render(<TodoPresentation todos={mockTodos} />);
+    const { getByText } = render(<TodoPresentation todos={[]} />);
     expect(getByText('Todo List')).toBeInTheDocument();
   });
   test('Todoが1件以上ある場合はTodo一覧が表示されること', () => {

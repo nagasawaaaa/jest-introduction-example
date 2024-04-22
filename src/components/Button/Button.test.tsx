6a3b47800a2acee453ea82a4180c3a65a910fe31
Satoshi Nagasawa <nagasawa@ham.works>
2024-04-22 21:44:45 +0900

    fix: Buttonコンポーネントのテストケース

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,12 +4,12 @@ import Button from './Button';
 
 describe('Button Render', () => {
   beforeEach(cleanup);
-  test('Button にテキストを渡せる', () => {
+  test('Button の children にテキストを渡せる', () => {
     const { getByRole } = render(<Button>Click me</Button>);
     const buttonElement = getByRole('button', { name: 'Click me' });
     expect(buttonElement).toBeInTheDocument();
   });
-  test('Button にクリックイベントを渡して発火できる', () => {
+  test('Button にクリックイベントを渡してコールできる', () => {
     const onClick = jest.fn();
     const { getByRole } = render(<Button onClick={onClick}>Click me</Button>);
     const buttonElement = getByRole('button', { name: 'Click me' });
